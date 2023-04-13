3f361a40e2d5272f7998dac377c685256021a4ae
Marshall Chan <marshallchan666@outlook.com>
2023-04-14 00:27:19 +0800

    feat: optimize the ball shadow

--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -1,7 +1,31 @@
+import { useRef } from "react";
+import { LightNode, useFrame } from "@react-three/fiber";
+import { Light, DirectionalLightHelper } from "three";
+import { useHelper } from "@react-three/drei";
+
 export default function Lights() {
+  const light = useRef<Light>();
+
+  useFrame((state) => {
+    // * Currently, the light is focusing on the camera, meaning that a significant part of the shadow map is used behind the camera, which is a waste.
+    // * We can fix that by moving the light and target forward a little with - 4:
+    // * 光向前照一点
+    // * light follow the camera
+    light.current!.position.z =
+      state.camera.position.z + 1 - 4;
+    // * adjust the light emission target
+    light.current!.target.position.z =
+      state.camera.position.z - 4;
+    // * put the light target in the scene
+    light.current.target.updateMatrixWorld();
+  });
+
+  useHelper(light, DirectionalLightHelper);
+
   return (
     <>
       <directionalLight
+        ref={light}
         castShadow
         position={[4, 4, 1]}
         intensity={1.5}
